b7c189dbdd039a5ad87ea1a2291ab4f0ae943380
AlexParog <66374853+AlexParog@users.noreply.github.com>
2021-04-22 23:27:46 +0300

    Mockito Tests

---
+++ src/NJC_MockitoTest_Task1/src/test/java/ru/netology/i18n/LocalizationServiceImplTest.java
@@ -0,0 +1,35 @@
+package ru.netology.i18n;
+
+import org.junit.jupiter.api.Test;
+import ru.netology.entity.Country;
+import ru.netology.entity.Location;
+import ru.netology.geo.GeoService;
+import ru.netology.geo.GeoServiceImpl;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LocalizationServiceImplTest {
+
+    private final GeoService geoService = new GeoServiceImpl();
+    private final LocalizationService localizationService = new LocalizationServiceImpl();
+
+    @Test
+    void locationRussia() {
+        Location location = geoService.byIp("172.0.32.11");
+        System.out.printf("Отправлено сообщение: %s", localizationService.locale(location.getCountry()));
+
+        String actual = "Добро пожаловать";
+
+        assertEquals(localizationService.locale(location.getCountry()), actual);
+    }
+
+    @Test
+    void otherLocations() {
+        Location location = geoService.byIp("96.44.183.149");
+        System.out.printf("Отправлено сообщение: %s", localizationService.locale(location.getCountry()));
+
+        String actual = "Welcome";
+
+        assertEquals(localizationService.locale(location.getCountry()), actual);
+    }
+}
